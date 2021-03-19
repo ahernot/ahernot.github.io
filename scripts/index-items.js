23d5c7a3c8d53da11bb73b0357479f7e0a1659d4
ahernot <70817470+ahernot@users.noreply.github.com>
2021-03-19 09:33:44 +0100

    Update index-items.js

--- scripts/index-items.js
+++ scripts/index-items.js
@@ -1,40 +1,68 @@
-let nbItems = 2;
+// let nbItems = 2;
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     for (itemId = 0; itemId < nbItems; itemId++) {
+//         let itemContainer = document.getElementById("item-" + itemId);
+//     }
+
+//     setTimeout(function () {
+//         itemDisplay(0, 200);
+//     }, 1600);
+
+// }, false);
 
 
-document.addEventListener('DOMContentLoaded', function() {
+// function itemDisplay(itemId, itemTimeout) {
 
-    for (itemId = 0; itemId < nbItems; itemId++) {
-        let itemContainer = document.getElementById("item-" + itemId);
-    }
+//     setTimeout(function() {
 
-    setTimeout(function () {
-        itemDisplay(0, 200); // Sequentially write message
-    }, 1600);
+//         let itemContainer = document.getElementById("item-" + itemId);
+//         console.log(itemId);
+//         itemContainer.classList.add("active");
 
-}, false);
+//         if (itemId < nbItems - 1) {
+//             itemDisplay(itemId + 1, itemTimeout);
+//         }
+//         // else if (i == nbItems) {
+//         //     introTextContainer.innerHTML = textBegin + text.substring(0, i) + textEnd;
+//         // }
+
+//     }, itemTimeout);
+// }
+
+var sheets = window.document.styleSheets;
+var sheet = sheets[2];
+// sheet.insertRule('strong { color: red; }', sheet.cssRules.length);
 
 
+let items = document.getElementsByClassName("item");
 
+for (var i = 0; i < items.length; i++) {
 
+    // Get class instances
+    let item = items[i];
+    let itemText = item.getElementsByClassName("item-text")[0];
+    
 
+    // Mouseover event
+    item.addEventListener("mouseover", function(event) {
+        // imageOverlay.classList.add("active");
+        //sheet.insertRule('body { backdrop-filter: blur(2px); }', sheet.cssRules.length);
+        document.body.style.backdropFilter = "blur(3px)";
+        itemText.style.color = "black";
 
-function itemDisplay(itemId, itemTimeout) {
+    });
 
-    setTimeout(function() {
-
-        let itemContainer = document.getElementById("item-" + itemId);
-        console.log(itemId);
-        itemContainer.classList.add("active");
-
-        if (itemId < nbItems - 1) {
-            itemDisplay(itemId + 1, itemTimeout);
-        }
-        // else if (i == nbItems) {
-        //     introTextContainer.innerHTML = textBegin + text.substring(0, i) + textEnd;
-        // }
-
-    }, itemTimeout);
-}
+    // Mouseleave event
+    item.addEventListener("mouseleave", function(event) {
+        // imageOverlay[0].classList.remove("active");
+        // sheet.insertRule('body { backdrop-filter: blur(0px) !important; }', sheet.cssRules.length);
+        document.body.style.backdropFilter = "";
+        itemText.style.color = "white";
+    });
+    
+};
 
 
 
