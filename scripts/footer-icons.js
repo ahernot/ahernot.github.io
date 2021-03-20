d55d695224a7967bcf71afb53f7badb6a8245a0b
ahernot <70817470+ahernot@users.noreply.github.com>
2021-03-20 22:56:57 +0100

    add dark mode support

--- scripts/footer-icons.js
+++ scripts/footer-icons.js
@@ -1,21 +1,45 @@
 let footerIcons = document.getElementsByClassName("footer-icon");
 
+let darkMode = false;
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkMode = true;
+}
+
 for (var i = 0; i < footerIcons.length; i++) {
 
     // Get class instances
     let iconContainer = footerIcons[i];
-    let iconMono = iconContainer.getElementsByClassName("icon-mono")[0];
+
+    // Get elements
+    let iconMonoLight = iconContainer.getElementsByClassName("icon-mono-light")[0];
+    let iconMonoDark = iconContainer.getElementsByClassName("icon-mono-dark")[0];
     let iconColor = iconContainer.getElementsByClassName("icon-color")[0];
+
+    // Check if wrong icon for current theme
+    console.log(iconMonoLight.classList);
+    if ((darkMode) && (iconMonoLight.classList.contains("active") >= 0)) {
+        iconMonoLight.classList.remove("active");
+        iconMonoDark.classList.add("active");
+    }
+    else if (!(darkMode) && (iconMonoDark.classList.contains("active") >= 0)) {
+        iconMonoLight.classList.add("active");
+        iconMonoDark.classList.remove("active");
+    }
 
     // Mouseover event
     iconContainer.addEventListener("mouseover", function(event) {
-        iconMono.classList.remove("active");
+        iconMonoLight.classList.remove("active");
+        iconMonoDark.classList.remove("active");
         iconColor.classList.add("active");
     });
 
     // Mouseleave event
     iconContainer.addEventListener("mouseleave", function(event) {
-        iconMono.classList.add("active");
+        if (darkMode) {
+            iconMonoDark.classList.add("active");
+        } else { // light mode or media queries unsupported
+            iconMonoLight.classList.add("active");
+        }
         iconColor.classList.remove("active");
     });
     
