0c0a25c9991c31e0ae94ab3f6f3669f9516f5362
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-09 17:42:43 +0200

    create FloorDict

--- backend/processing/auxiliary_functions.py
+++ backend/processing/auxiliary_functions.py
@@ -22,3 +22,35 @@ def imread_rgba(path):
             axis=2
         )
     return img
+
+
+class FloorDict:
+    """
+    Propagates value until next keypoint
+    included on left, excluded on right
+    """
+
+    def __init__(self, breaks: list):
+        # breaks = [ (0,10) , (1,0) , (7, 15) , (12,70) , (14,-4) ]
+
+        breaks.sort()
+        self.__keypoints, self.__keyvals = zip(*breaks)
+        self.__len = len( self.__keypoints )
+
+    def __getitem__(self, key: int or float):
+
+        for i in range(self.__len - 1):
+            bound_left = self.__keypoints [i]
+            bound_right = self.__keypoints [i + 1]
+
+            if key >= bound_left and key < bound_right:
+                return self.__keyvals[i]
+        
+        if key >= self.__keypoints [-1]:
+            return self.__keyvals [-1]
+
+        # Key on the left of first point
+        return None
+
+# f = FloorDict( [ (0,10) , (1,0) , (7, 15) , (12,70) , (14,-4) ] )
+# print(f[0.2023])
