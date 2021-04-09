6145f625930199ed13035f06dd5c156803c4af3f
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-09 13:03:44 +0200

    create clamp()

--- backend/processing/auxiliary_functions.py
+++ backend/processing/auxiliary_functions.py
@@ -5,3 +5,7 @@ def generate_dir(dirpath: str):
         os.makedirs(dirpath)
     except FileExistsError:
         pass
+
+def clamp(val, min_val, max_val):
+    if min_val > max_val: min_val, max_val = max_val, min_val
+    return max(min_val, min(val, max_val))
