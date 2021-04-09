8b7b4a4ab48af9766fb1e9b390aef90ad8440292
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-09 16:51:08 +0200

    Update preferences.py

--- backend/processing/preferences.py
+++ backend/processing/preferences.py
@@ -1,2 +1,5 @@
 SOURCE_PATH = '/Users/anatole/Desktop/website-resources/_sampleDir'  # '/Users/anatole/Desktop/website-resources/albums/'
 EXTENSIONS = ['jpg', 'jpeg', 'tiff', 'png', 'heic', 'heif']
+SIZES = {
+    '': 0
+}
