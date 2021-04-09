566dbe1caecab0c7b240e6a2bf6be057e696dc8e
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-09 18:11:10 +0200

    add watermarking prefs

--- backend/processing/preferences.py
+++ backend/processing/preferences.py
@@ -1,5 +1,15 @@
 SOURCE_PATH = '/Users/anatole/Desktop/website-resources/_sampleDir'  # '/Users/anatole/Desktop/website-resources/albums/'
 EXTENSIONS = ['jpg', 'jpeg', 'tiff', 'png', 'heic', 'heif']
-SIZES = {
-    '': 0
-}
+
+from auxiliary_functions import FloorDict
+SIZES = [
+    (0.0, 1200),  # aspect ratio ≥0.0  =>  size = 1200px
+    (2.5, 2000)  # aspect ratio ≥2.5  =>  size = 2000px
+]
+SIZES_FD = FloorDict( SIZES )
+
+WATERMARKS = [
+    (0.1, 0.0, 1.0, 0.3),  # bottom left
+    (0.1, 0.5, 0.5, 0.3),  # middle
+    (0.1, 1.0, 0.0, 0.3)  # top right
+]
