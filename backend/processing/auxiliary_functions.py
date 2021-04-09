692aecdc4088d44c97bc1fc0d9f18caa4126d08d
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-09 16:20:59 +0200

    add imread_rgba()

--- backend/processing/auxiliary_functions.py
+++ backend/processing/auxiliary_functions.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+import numpy as np
 
 def generate_dir(dirpath: str):
     try:
@@ -9,3 +11,14 @@ def generate_dir(dirpath: str):
 def clamp(val, min_val, max_val):
     if min_val > max_val: min_val, max_val = max_val, min_val
     return max(min_val, min(val, max_val))
+
+def imread_rgba(path):
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    height, width, depth = img.shape
+    if depth == 3:
+        img = np.concatenate((
+            img,
+            np.ones((height, width, 1), dtype=img.dtype) * 255),
+            axis=2
+        )
+    return img
