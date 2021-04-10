bb50ab59e8e8b4aeaca472773e8b0cffa404c5a9
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-10 16:51:29 +0200

    struggle with asynchronous/synchronous functions

--- backend/loading/load-tests.js
+++ backend/loading/load-tests.js
@@ -10,30 +10,6 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
-//usage:
-// readTextFile("resources/albums/_tests/_sampleAlbum/myFile.json", function(text){
-//     var data = JSON.parse(text);
-//     console.log(data);
-// });
-
-
-let testContainer = document.getElementById("test-msg");
-
-document.addEventListener('DOMContentLoaded', function() {
-
-    readTextFile("../../resources/albums/_tests/_sampleAlbum/myFile.json", function(text) { // "resources/albums/_tests/_sampleAlbum/myFile.json
-        var data = JSON.parse(text);
-        for (key in data) {
-            console.log(key);
-            // val = data[key]
-            // testContainer.innerHTML += key + ": " + val + "<br>";
-        }
-    });
-}, false);
-
-
-
-
 
 
 
@@ -45,56 +21,94 @@ document.addEventListener('DOMContentLoaded', function() {
 // etc
 
 
-
-
-
-
-var elem = `<div class="content-container" id="image-1-1">
-<div class="image-container w2-h3">
-    <img src="../../resources/albums/urban-photography/2019-08-02_montmartre-batignolles_original-watermarked.png" alt=""></img>
-    <div class="image-overlay" style="background-image: url('../../resources/albums/urban-photography/2019-08-02_montmartre-batignolles_original-watermarked-mask0.png'); background-size: cover;"></div>
-    <div class="image-overlay" style="background-image: url('../../resources/albums/urban-photography/2019-08-02_montmartre-batignolles_original-watermarked-mask1.png'); background-size: cover;"></div>
-    <div class="image-overlay" style="background-image: url('../../resources/albums/urban-photography/2019-08-02_montmartre-batignolles_original-watermarked-mask2.png'); background-size: cover;"></div>
-
-
-    <div class="info-overlay">
-        <div class="info-header">Location</div>
-        <div class="info-date">Date</div>
-
-        <div class="info-camera">
-            <div class="setting-icon-container">
-                <img src="../../resources/icons/icon-camera.svg" class="info-icon"></img>
-            </div>
-            <div class="val">Camera</div>
-        </div>
-
-        <ul class="info-settings">
-            <li>
-                <div class="setting-icon-container">
-                    <img src="../../resources/icons/icon-iso-black.png" class="info-icon" alt="ISO">
-                </div>
-                <div class="val">0</div>
-            </li>
-            <li>
-                <div class="setting-icon-container">
-                    <img src="../../resources/icons/icon-shutterspeed-black.png" class="info-icon" alt="Shutter Speed">
-                </div>
-                <div class="val">0</div>
-            </li>
-            <li>
-                <div class="setting-icon-container">
-                    <img src="../../resources/icons/icon-aperture-black.png" class="info-icon" alt="Aperture">
-                </div>
-                <div class="val">f/0</div>
-            </li>
-        </ul>
-    </div>
-
-</div>
-</div>
-`;
-
 let container = document.getElementById("main-container");
 let col1 = container.getElementsByClassName("col-left") [0];
 let col2 = container.getElementsByClassName("col-right") [0];
-col1.innerHTML += elem;
+
+let albumDir = '../../resources/albums/_tests/_sampleAlbum/'
+
+function loadImage (imageDir) {
+    console.log('Loading image');
+
+    let imageSize = 'high';
+
+    // Get paths
+    let imagePath = imageDir + 'image-' + imageSize + '.png';
+    let maskPath1 = imageDir + 'mask-' + imageSize + '-1.png';
+    let maskPath2 = imageDir + 'mask-' + imageSize + '-2.png';
+    let maskPath3 = imageDir + 'mask-' + imageSize + '-3.png';
+    let jsonPath = imageDir + 'info.json';
+
+    // Read JSON
+    readTextFile(jsonPath, function(text) {
+        let dataDict = JSON.parse(text);
+    });
+    
+    let dataDict = {'location': 'xx'}; // fix
+
+    var elem = `<div class="content-container" id="image-1-1">
+    <div class="image-container w2-h3">
+        <img src="${imagePath}" alt=""></img>
+        <div class="image-overlay" style="background-image: url('${maskPath1}'); background-size: cover;"></div>
+        <div class="image-overlay" style="background-image: url('${maskPath2}'); background-size: cover;"></div>
+        <div class="image-overlay" style="background-image: url('${maskPath3}'); background-size: cover;"></div>
+    
+    
+        <div class="info-overlay">
+            <div class="info-header">${dataDict['location']}</div>
+            <div class="info-date">Date</div>
+    
+            <div class="info-camera">
+                <div class="setting-icon-container">
+                    <img src="../../resources/icons/icon-camera.svg" class="info-icon"></img>
+                </div>
+                <div class="val">Camera</div>
+            </div>
+    
+            <ul class="info-settings">
+                <li>
+                    <div class="setting-icon-container">
+                        <img src="../../resources/icons/icon-iso-black.png" class="info-icon" alt="ISO">
+                    </div>
+                    <div class="val">0</div>
+                </li>
+                <li>
+                    <div class="setting-icon-container">
+                        <img src="../../resources/icons/icon-shutterspeed-black.png" class="info-icon" alt="Shutter Speed">
+                    </div>
+                    <div class="val">0</div>
+                </li>
+                <li>
+                    <div class="setting-icon-container">
+                        <img src="../../resources/icons/icon-aperture-black.png" class="info-icon" alt="Aperture">
+                    </div>
+                    <div class="val">f/0</div>
+                </li>
+            </ul>
+        </div>
+    
+    </div>
+    </div>
+    `;
+
+    // Add element
+    col1.innerHTML += elem;
+
+
+
+};
+
+
+let imageDir = '../../resources/albums/_tests/_sampleAlbum/2019-08-02_montmartre-batignolles_original/'
+    loadImage(imageDir);
+
+
+// Run script
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     let imageDir = '../../resources/albums/_tests/_sampleAlbum/2019-08-02_montmartre-batignolles_original/'
+//     loadImage(imageDir);
+
+//     // Trigger finished-loading event
+//     var ev = new CustomEvent("finished-loading", { "detail": "Example of an event" });
+//     document.dispatchEvent(ev);
+// });
--- scripts/album-overlay.js
+++ scripts/album-overlay.js
@@ -1,10 +1,23 @@
+
 let imageContainers = document.getElementsByClassName("image-container");
+
+// Always actualise:
+// document.addEventListener("finished-loading", function(e) {
+//     let imageContainers = document.getElementsByClassName("image-container");
+//     console.log('finished loading');
+//     console.log(imageContainers);
+//     console.log(imageContainers.length);
+// });
 
 for (var i = 0; i < imageContainers.length; i++) {
 
     // Get class instances
     let imageContainer = imageContainers[i];
+
     let infoOverlay = imageContainer.getElementsByClassName("info-overlay")[0];
+    // document.addEventListener("finished-loading", function(e) {
+    //     infoOverlay = imageContainer.getElementsByClassName("info-overlay")[0];
+    // });
 
     // Continue loop if no matching subclass
     if (infoOverlay == undefined) {
